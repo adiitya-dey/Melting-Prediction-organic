671293f70a1c5fef50fa81695cbd6aae21d5fa4b
aditya.dey <adiitya.dey@gmail.com>
2023-09-27 15:53:58 +0200

    dbt script test 1

--- docker/dbt/bradley_clean_data/app.py
+++ docker/dbt/bradley_clean_data/app.py
@@ -1,12 +1,20 @@
 import flask
-
+import subprocess
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<h1>Hello World</h1>'
+    try:
+        subprocess.run(['sh', 'scripts.sh'], check=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running the shell script: {e}")
+    else:
+        print(f"Shell script executed successfully.")
+
+
+    return '<h1>DBT triggered</h1>'
 
 
 if __name__=="__main__":
