2c2707e1f271f3e9fcbbf5341a56b727670969a6
aditya.dey <adiitya.dey@gmail.com>
2023-09-28 03:01:41 +0200

    dbt script test 20

--- docker/dbt/bradley_ETL/scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-# dbt debug --target dev --profiles-dir bradley_clean_data/.
-dbt debug --target prod --profiles-dir .
-dbt run --target prod --profiles-dir .
-# dbt test --data --target prod --profiles-dir .
