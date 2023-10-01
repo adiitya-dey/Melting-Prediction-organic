4ac03341ce7ba06ea1a47404f3eb468b240a8844
aditya.dey <adiitya.dey@gmail.com>
2023-10-01 20:40:06 +0200

    sdf conversion test 1

--- docker/sdf_converter/main.py
+++ docker/sdf_converter/main.py
@@ -1,20 +1,46 @@
 import flask
 import subprocess
+import os
+from google.cloud import storage
+from flask import request
 
 app = flask.Flask(__name__)
 
 
+def upload_blob(file_name):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/secrets/storage-creator"
+    client = storage.Client()
+    bucket_name = 'datasets-meltingpointprediction'
+    source_file_name =  file_name
+    destination_blob_name = "SDF/" + file_name
+
+    try:
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+    except blob.Error as e:
+        return f"Error uploading the file: {e}"
+    else:
+        return f"{source_file_name} uploaded successfully to {destination_blob_name}."
+    
+
+
 @app.route('/', methods=['GET'])
 def home():
+    smiles = request.args.get('smiles')
+    key = request.args.get('key')
+    request.encoding_errors
+
     try:
-        # subprocess.run(['dbt','deps', '--profiles-dir','.', '--project-dir', '/'], check=True, text=True)
-        # subprocess.ßrun(['dbt','debug','--target','prod', '--profiles-dir','.', '--project-dir', 'bradley_clean_data/'], check=True, text=True)
-        subprocess.run(['dbt','run','--target','prod', '--profiles-dir','.', '--project-dir', 'open_melting_point_dataset/'], check=True, text=True)
+        file_name = key + '.sdf'
+        subprocess.run(['obabel','-:',smiles,'-O', file_name, '--seperate', '--unique', '--gen3D'], \
+                       check=True, text=True)
     except subprocess.CalledProcessError as e:
         return f"Error running the subprocess: {e}"
     else:
-        return f"Subprocess executed successfully."
+        upload_blob(file_name)
 
+    
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8080)
