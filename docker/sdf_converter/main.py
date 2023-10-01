4379fef81e64dd2877084e30f7c79360bac5036a
aditya.dey <adiitya.dey@gmail.com>
2023-10-01 19:32:45 +0200

    sdf conversion prepration

---
+++ docker/sdf_converter/main.py
@@ -0,0 +1,20 @@
+import flask
+import subprocess
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    try:
+        # subprocess.run(['dbt','deps', '--profiles-dir','.', '--project-dir', '/'], check=True, text=True)
+        # subprocess.ßrun(['dbt','debug','--target','prod', '--profiles-dir','.', '--project-dir', 'bradley_clean_data/'], check=True, text=True)
+        subprocess.run(['dbt','run','--target','prod', '--profiles-dir','.', '--project-dir', 'open_melting_point_dataset/'], check=True, text=True)
+    except subprocess.CalledProcessError as e:
+        return f"Error running the subprocess: {e}"
+    else:
+        return f"Subprocess executed successfully."
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080)
