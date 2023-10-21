1cdad30efe6b08f622c2f50091996aff1d29b1fc
aditya.dey <adiitya.dey@gmail.com>
2023-10-21 10:51:17 +0200

    sdf converter test 1

--- docker/sdf_converter/main.py
+++ docker/sdf_converter/main.py
@@ -1,80 +1,97 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-# import subprocess
-# import os
+import subprocess
+import os
 from google.cloud import storage
 # from flask import request, make_response, jsonify
-import uvicorn
+# import uvicorn
 
 app = FastAPI()
 
 
-# def upload_blob(file_name):
-#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/secrets/storage-creator"
-#     client = storage.Client()
-#     bucket_name = 'datasets-meltingpointprediction'
-#     source_file_name =  file_name
-#     destination_blob_name = "SDF/" + file_name
+def upload_blob(file_name):
+
+    # Provide path for credentials.
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/secrets/storage-creator"
+
+    # Initiate connection with Cloud.
+    client = storage.Client()
+
+    # Bucket Name
+    bucket_name = 'datasets-meltingpointprediction'
+
+    # Source and Destination file names.
+    source_file_name =  file_name
+    destination_blob_name = "SDF/" + file_name
     
-#     try:
-#         bucket = client.bucket(bucket_name)
-#         blob = bucket.blob(destination_blob_name)
-#         blob.upload_from_filename(source_file_name)
-#     except Exception as e:
-#         return f"Error uploading the file: {e}"
-#     else:
-#         return f"{source_file_name} uploaded successfully to {destination_blob_name}."
+    try:
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+    except Exception as e:
+        raise HTTPException(status_code=501,
+                            detail=[{"msg":f"Upload failed to bucket for {source_file_name}."
+                                     }])
+    else:
+        return f"{source_file_name} uploaded successfully to {destination_blob_name}."
+    
+
 
 class Smiles(BaseModel):
     key: str
     smiles: str   
 
 
-@app.get('/api/smilestosdf/')
-async def smilestopdf():
-    
-    return f"Successfullly"
+@app.post('/api/smilestosdf/', status_code=201)
+async def smiles_to_sdf(smiles: Smiles):
+
+    # Set the sdf file name based on key.
+    file_name = smiles.key + '.sdf'
+
+    # Set the smiles to input to open babel.
+    smile_name = '-:' + smiles.smiles
 
 
+    # Run the open babel to convert smiles to sdf file.
+    try:
 
-
-
-
-
-#     # Validate if the parameters for key and smiles are not empty.
-#     data = request.get_json()
-#     if data["key"] is None or data["smiles"] is None:
-#         message = {'message': f'Invalid Parameter passed.'}
-#         return message, 400
-    
-#     # Validate if the parameters for the key and smiles are 
-#     # string format.
-#     if type(data["key"]) != str or type(data["smiles"]) != str:
-#         message = {'message': f'Invalid Parameter passed for smiles.'}
-#         return message, 400
-    
-
-#     # Set the sdf file name based on key.
-#     file_name = data["key"] + '.sdf'
-
-#     # Set the smiles to input to open babel.
-#     smile_name = '-:' + data["smiles"]
-
-#     # Run the open babel to convert smiles to sdf file.
-#     result = subprocess.run(['obabel', smile_name,'-O', file_name, '--seperate', '--unique', '--gen3D'], \
-#                     check=True, text=True, capture_output=True)
-    
-#     # Check status of open babel conversion.
-#     if result.returncode == 0:
-
-#         # Check if open babel converted smiles to 1 molecule.
-#         if "0 molecules converted" in result.stdout:
-#             message =  {"message": f"Molecule did not convert successfully. Error: {result.stdout}"}
-#             return make_response(jsonify(message), 500)
+        result = subprocess.run(['obabel', smile_name,'-O', file_name, '--seperate', '--unique', '--gen3D'], \
+                        check=True, text=True, capture_output=True)
         
-#     else:
-#         message =  {"message": f"Open babel failed conversion. Error: {result.stderr}"}
-#         return make_response(jsonify(message), 500)
+    except Exception as e:
+        raise HTTPException(status_code=500, 
+                            detail=[{"msg": "obabel subprocess failed.",
+                                    "error": e}]
+                            )
+    
+    # Check if file is created.
+    if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
+        return upload_blob(file_name)
+    else:
+        raise HTTPException(status_code=500, 
+                            detail=[{"msg": f"File does not exists. Conversion failed. Run the obabel for smiles:{smiles.smiles} to check the error."}]
+                            )
+        
+    
+    
+        # # Check status of open babel conversion.
+        # if result.returncode == 0:
+
+        #     # Check if open babel converted smiles to 1 molecule.
+        #     if "0 molecules converted" in result.stdout:
+        #         raise HTTPException(status_code=500,
+        #                             detail=[{"msg": f"{smiles.smiles} Molecule did not convert.",
+        #                             "error": result.stdout}]
+        #                             )
+            
+        #     elif "1 molecule converted" in result.stdout:
+        #         return f"{file_name} created."
+            
+        # else:
+
+        #     raise HTTPException(status_code=500,
+        #                         detail=[{"msg": "obabel status is failed."}]
+        #                         )
 
     
 
