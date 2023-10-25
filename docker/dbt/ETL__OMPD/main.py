5f1eb94a40813a36df42c5bb925c133357ec113e
aditya.dey <adiitya.dey@gmail.com>
2023-10-26 00:00:10 +0200

    dbt test seed v1

--- docker/dbt/ETL__OMPD/main.py
+++ docker/dbt/ETL__OMPD/main.py
@@ -5,14 +5,26 @@ app = FastAPI()
 
 
 @app.get('/api/v1/dbt/run/ompd/', status_code=201)
-def home():
+async def home():
     try:
         # subprocess.run(['dbt','deps', '--profiles-dir','.', '--project-dir', '/'], check=True, text=True)
         # subprocess.ßrun(['dbt','debug','--target','prod', '--profiles-dir','.', '--project-dir', 'bradley_clean_data/'], check=True, text=True)
         subprocess.run(['dbt','run','--target','dev', '--profiles-dir','.', '--project-dir', 'open_melting_point_dataset/'], check=True, text=True)
     except subprocess.CalledProcessError as e:
-        raise HTTPException(status_code=501,
-                            detail=[{'msg': "Subprocess failed to run dbt.",
-                                     'error': f"{e}"}]) 
+        raise HTTPException(status_code=500,
+                            detail=[{'msg': "Subprocess failed to run dbt." }]
+                            ) 
+    else:
+        return 0
+    
+
+@app.get('/api/v1/dbt/run/ompd/seed/', status_code=201)
+async def seed():
+    try:
+        subprocess.run(['dbt','seed','--target','dev', '--profiles-dir','.', '--project-dir', 'open_melting_point_dataset/'], check=True, text=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500,
+                            detail=[{'msg': "Subprocess failed to run dbt seed."}]
+                            )
     else:
         return 0
